09a8fc775c8097b770a8e95e0b46c485345f5dcd
Faisal Haroon <faisal.haroon@hytgenx.com>
2025-03-21 01:51:28 +0500

    build second

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export', // Enables static export mode
+    images: {
+      unoptimized: true, // Required for static exports if using Next.js Image component
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ out/_next/static/chunks/app/blog/blog-details/page-844fa0a5832c0c3a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,281,555,831],{7413:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23)),Promise.resolve().then(n.bind(n,6432))}},e=>{var s=s=>e(e.s=s);e.O(0,[564,432,441,517,358],()=>s(7413)),_N_E=e.O()}]);
